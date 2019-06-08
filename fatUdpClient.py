a37365d6b5d7caeba6349a79f44fa81e9af2032d
ASukhanov <sukhanov@bnl.gov>
2019-06-07 22:32:20 -0400

    repair

--- fatUdpClient.py
+++ fatUdpClient.py
@@ -8,7 +8,9 @@ from timeit import default_timer as timer
 
 __version__ = 'v01 2019-06-05'# works at 5 MB/s for 36MB object
 #TODO: keep track of lost chunks and re-request them
+__version__ = 'v02 2019-06-06'# Acknowledge after EOD
 
+<<<<<<< HEAD
 def ip_address():
     """Platform-independent way to get local host IP address"""
     return [(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close())\
@@ -22,17 +24,47 @@ PrefixLength = 2
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 #sock.settimeout(0.5)
+=======
+PrefixLength = 2
+
+def ip_address():
+    """Platform-independent way to get local host IP address"""
+    return [(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close())\
+      for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]
+
+import argparse
+parser = argparse.ArgumentParser(
+  description = 'Reciving client of a fatUdpServer')
+parser.add_argument('-p','--port',type=int,default=9998,
+  help='Port number')
+parser.add_argument('-H','--host',default=ip_address(),nargs='?',
+  help='Hostname')
+parser.add_argument('request', nargs='?', 
+  help='Request to server')
+pargs = parser.parse_args()
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#sock.settimeout(0.5)
+#sock.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,1)
+#sock.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,10*1024*1024)
+#print(sock.getsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF))
+>>>>>>> 42df0eeb51ba0c1b7159911411b27eaed103b593
 
 prefix = 1
 buff = b''
 prevPrefix = None
 chunkSize = 65000
+<<<<<<< HEAD
+=======
+ichunk = 0
+>>>>>>> 42df0eeb51ba0c1b7159911411b27eaed103b593
 '''recvfrom_into()
 maxChunks = 1000
 chunks = bytearray(maxChunks*chunkSize)
 chunkNBytes = [0]*maxChunks
 bufptr = 0
 '''
+<<<<<<< HEAD
 sock.sendto(bytes(data + "\n", "utf-8"), (HOST, PORT))
 print('Sent %s'%data)
 
@@ -44,6 +76,26 @@ while prefix:
     if len(received) <= 3:
         print('EOD')
         break 
+=======
+print('Request sent to '+pargs.host+':%i'%pargs.port)
+data = bytes(pargs.request, "utf-8")
+sock.sendto(data, (pargs.host, pargs.port))
+print('Sent %s'%data)
+
+ts = timer()
+nTries = 1
+while prefix:
+    received, addr = sock.recvfrom(chunkSize)
+    if len(received) == 3: # and received == b'EOD':
+        print('EOD',prefix,nTries)
+        if prefix and nTries:
+            print('got EOD, but %i packets are missed, tries#%i'%(prefix,nTries))
+            nTries -= 1
+            continue
+        sock.sendto(b'ACK',(pargs.host, pargs.port))
+        break 
+    ichunk += 1
+>>>>>>> 42df0eeb51ba0c1b7159911411b27eaed103b593
     prefix = int.from_bytes(received[:PrefixLength],'big')
     if prevPrefix is None:
         prevPrefix = prefix + 1
@@ -57,7 +109,14 @@ while prefix:
     #print('received %i bytes from %s'%(len(data),addr))
     #print(prefix)
     buff = b''.join([buff,data])
+<<<<<<< HEAD
     ichunk += 1
+=======
+
+if prefix == 0:
+    print('Success')
+    sock.sendto(b'ACK',(pargs.host, pargs.port))
+>>>>>>> 42df0eeb51ba0c1b7159911411b27eaed103b593
 
 '''recvfrom_into()
 # The recvfrom_into() is surprisingly Very slow
